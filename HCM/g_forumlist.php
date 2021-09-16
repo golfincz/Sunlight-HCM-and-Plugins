acf1b195477967a5e6bae574e5f898042fac7829
golfin <golfin.martin.randa@gmail.com>
2021-09-16 05:40:31 +0200

    Update g_forumlist.php

--- HCM/g_forumlist.php
+++ HCM/g_forumlist.php
@@ -1,14 +1,13 @@
 <?php
 
+use Sunlight\Post\PostService;
 use Sunlight\Database\Database as DB;
 use Sunlight\GenericTemplates;
-use Sunlight\Comment\Comment;
+use Sunlight\Post\Post;
 use Sunlight\Router;
 use Sunlight\User;
 use Sunlight\Util\Arr;
 use Sunlight\Util\StringManipulator;
-
-defined('_root') or exit;
 
 return function ($limit = null, $stranky = "", $typ = null) {
     // priprava
@@ -18,48 +17,48 @@ return function ($limit = null, $stranky = "", $typ = null) {
     } else {
         $limit = 10;
     }
-    $post_types =  array(
-        'topic' => _post_forum_topic
-    );
+    $post_types =  [
+        'topic' => Post::FORUM_TOPIC,
+    ];
 
     // nastaveni filtru
     if (!empty($stranky)) {
         $homes = Arr::removeValue(explode('-', $stranky), '');
     } else {
-        $homes = array();
+        $homes = [];
     }
 
     if (!empty($typ)) {
         if (isset($post_types[$typ])) {
             $typ = $post_types[$typ];
         } elseif (!in_array($typ, $post_types)) {
-            $typ = _post_section_comment;
+            $typ = Post::SECTION_COMMENT;
         }
-        $types = array($typ);
+        $types = [$typ];
     } else {
         $types = $post_types;
     }
 
     // dotaz
-    list($columns, $joins, $cond) = Comment::createFilter('post', $types, $homes);
+    [$columns, $joins, $cond] = Post::createFilter('post', $types, $homes);
     $userQuery = User::createQuery('post.author');
     $columns .= ',' . $userQuery['column_list'];
     $joins .= ' ' . $userQuery['joins'];
-    $query = DB::query("SELECT " . $columns . " FROM " . _comment_table . " post " . $joins . " WHERE " . $cond . " ORDER BY id DESC LIMIT " . $limit);
+    $query = DB::query("SELECT " . $columns . " FROM " . DB::table('post') . " post " . $joins . " WHERE " . $cond . " ORDER BY id DESC LIMIT " . $limit);
 
     while ($item = DB::row($query)) {
-        list($homelink, $hometitle) = Router::post($item);
+        [$homelink, $hometitle] = Router::post($item);
 
         if ($item['author'] != -1) {
             $authorname = Router::userFromQuery($userQuery, $item);
         } else {
-            $authorname = $item['guest'];
+            $authorname = PostService::renderGuestName($item['guest']);
         }
 
 
 		$result .= "<div class='topic-latest'>\n";
-		$result .= "<a href='" . $homelink . "'>" . $hometitle . "</a>\n";
-		$result .= "<em>" . _lang('global.postauthor') . ": " .  $authorname . "</em>, \n";
+		$result .= "<a href='" . _e($homelink) . "'>" . $hometitle . "</a>\n";
+		$result .= "<em>" . _lang('global.postauthor') . ": " .  $authorname . "</em>\n";
 		$result .= "<em>" . GenericTemplates::renderTime($item['time'], 'post') . "</em>\n";
         $result .= "</div>\n\n";
     }
