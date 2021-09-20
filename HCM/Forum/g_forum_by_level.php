14250e820437022bf72f1e6a44b6aca2d93a9d83
golfin <golfin.martin.randa@gmail.com>
2021-09-20 04:57:20 +0200

    Update g_forum_by_level.php

--- HCM/Forum/g_forum_by_level.php
+++ HCM/Forum/g_forum_by_level.php
@@ -6,7 +6,7 @@
 // Zároveň přidány poslední odpovědi v jednom výpisu pro tabulku Fora (není hotové)
 // Created by Golfin eranova.cz
 // ******************************************************************
-// Poslední aktualizace souboru: 17.9.2021
+// Poslední aktualizace souboru: 20.9.2021
 
 use Sunlight\Hcm;
 use Sunlight\User;
@@ -20,7 +20,7 @@ return function ($min_uroven = 0, $result = "", $nevyhovujici_text = "") {
          $result .= "<td class='topic-icon-cell'><a href='/sunlight/forum/skryta-sekce/'><img src='/sunlight/plugins/templates/eranova/images/icons/topic-normal.png' alt='Téma bez odpovědí' /></a></td>
 				<td class='topic-main-cell'><a href='/sunlight/forum/skryta-sekce/'><strong>Skrytá sekce</strong></a><br /><small>Viditelné pro uživatele s úrovní 5000</small></td>\n";
 		 $result .= "<td>\n";
-		 $result .= Sunlight\Hcm::run('g_last,1,8-10');
+		 $result .= Sunlight\Hcm::run('g_last_level');	// vytvořen extra HCM g_last_level pro toto konkrétní skryté fórum
 		 $result .= "</td>\n";
          $result .= "</tr>\n";
          $result .= "</tbody>\n";
